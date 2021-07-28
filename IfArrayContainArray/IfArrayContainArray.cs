45cc17b90f59cd9250539b62e1162b0d25c4020b
sergeklokov <serge.klokov@gmail.com>
2021-07-28 13:39:31 -0700

    Initial commit

---
+++ IfArrayContainArray/IfArrayContainArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfArrayContainArray
+{
+    class IfArrayContainArray
+    {
+        static void Main(string[] args)
+        {
+            // [10:18 AM] Fei Cheng
+            //        arrayA:[5, 1, 22, 25, 6, -1, 8, 10]
+            //arrayB:[1, 6, -1, 10]
+            //​[10:19 AM] Fei Cheng
+            //    arrayC:[5, 25, 22, 8]
+
+
+
+            int[] a = { 5, 1, 22, 25, 6, -1, 8, 10 };
+            int[] b = { 1, 6, -1, 10 };   // O( n x m ) O (m)? 
+            //int[] b = { 5, 25, 22, 8 };
+
+
+            var r = AreArraysInclusiveSolution1(a, b);
+
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+
+        static bool AreArraysInclusiveSolution1(int[] a, int[] b)
+        {
+            bool res = false;
+            int pos = 0;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                bool tempRes = false;
+                for (int j = pos; j < a.Length; j++)
+                {
+                    if (a[j] == b[i])
+                    {
+                        tempRes = true;
+                        pos = j;
+                        break;
+                    }
+                }
+
+                res = tempRes;
+                if (!tempRes)
+                    return false;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Very simple exercise to get max performance search of array in another array.
+
+thank you
+sam klok
